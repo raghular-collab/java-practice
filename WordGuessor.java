42760831007c52e964fb728935c4e40ea49cec9e
raghular-collab <raghul.chitra@gmail.com>
2026-02-17 16:22:23 +0530

    Add files via upload

---
+++ WordGuessor.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class WordGuessor {
+    public static void main(String[] args) {
+    
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Welcome to the Word Guessor Game!");
+    System.out.println("You have 5 attempts to guess the correct word");
+    System.out.println("Guess the Word!");
+
+    int i = 0;
+    String isWord = "Coder"; 
+    
+    for(i = 0; i<5; i++) {
+        System.out.println("Enter your word!");
+        String newWord = scanner.nextLine();
+        if (newWord.equals(isWord)) {
+            System.out.println("Word is correct!");
+        }
+        else {
+            System.out.println("Word is Wrong");
+        }
+        }
+        scanner.close();
+    }
+    
+}
